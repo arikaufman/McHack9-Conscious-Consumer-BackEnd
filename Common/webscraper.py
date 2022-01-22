f7ae9f05a63e4d9715f580bec84ad934a804135d
nikakp <nikakp@gmail.com>
2022-01-22 16:14:34 -0500

    added stuff to webscraper

--- Common/webscraper.py
+++ Common/webscraper.py
@@ -1,38 +1,16 @@
 import requests
-from html.parser import HTMLParser
-from HTMLParser import HTMLParser
+#from html.parser import HTMLParser
+#from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
 
-URL = "https://www.sustainalytics.com/esg-ratings/amazon-com-inc/1007896995"
-page = requests.get(URL)
-print(page.text)
+URL_homepage = "https://www.sustainalytics.com/esg-ratings"
+html_home = requests.get(URL_homepage)
+URL_company = "https://www.sustainalytics.com/esg-rating/cisco-systems-inc/1007897937"
+html_company = requests.get(URL_company)
 
-class FindRating(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.inLink = False
-        self.dataArray = []
-        self.countLanguages = 0
-        self.lasttag = None
-        self.lastname = None
-        self.lastvalue = None
+print(html_company.text)
 
-    def handle_starttag(self, tag, attrs):
-        self.inLink = False
-        if tag == 'div':
-            for name, value in attrs:
-                if name == 'class' and value == 'col-6 risk-rating-score':
-                    self.countLanguages += 1
-                    self.inLink = True
-                    self.lasttag = tag
+soup = BeautifulSoup(html_company.content, "html.parser")
+rating = soup.find('div', {"class" : "col-6 risk-rating-score"})
+print(rating.text)
 
-    def handle_endtag(self, tag):
-        if tag == "div":
-            self.inlink = False
-
-    def handle_data(self, data):
-        if self.lasttag == 'div' and self.inLink and data.strip():
-            print(data)
-
-parser = FindRating()
-parser.feed(page.text)
