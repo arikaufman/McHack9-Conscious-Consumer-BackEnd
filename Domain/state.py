621ea2facdfdd91ade8b020e151ca40fc4cc68be
arikaufman <42683887+arikaufman@users.noreply.github.com>
2022-01-22 16:11:11 -0500

    route2

---
+++ Domain/state.py
@@ -0,0 +1,13 @@
+class State:
+    def __init__(
+        self,
+        name="",
+        item="",
+        longitude="",
+        latitude=""):
+
+        self.name = name
+        self.item = item
+        self.longitude = longitude
+        self.latitude = latitude
+
---
+++ Domain/stateDomain.py
@@ -0,0 +1,5 @@
+import state
+
+def get(state):
+   state.name = "test"
+   return state
